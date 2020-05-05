5286d1d5855179d8a15a87cfba0f2b6cdca007f4
Bolotov Maxim <maxim.bolotov.1997@gmail.com>
2020-05-05 23:24:31 +0300

    Authorization

--- js/main.js
+++ js/main.js
@@ -2,12 +2,90 @@ const cartButton = document.querySelector('#card-button');
 const modal = document.querySelector('.modal');
 const close = document.querySelector('.close');
 
-cartButton.addEventListener('click', toggleModal)
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const loginForm = document.querySelector('#logInForm');
+const loginInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
 
-close.addEventListener('click', toggleModal)
+let login = localStorage.getItem('myUsers');
+
+new WOW().init();
+
+cartButton.addEventListener('click', toggleModal);
+
+close.addEventListener('click', toggleModal);
 
 function toggleModal() {
    modal.classList.toggle('is-open');
 }
 
-new WOW().init();
+////
+
+function toggleModalAuth() {
+   modalAuth.classList.toggle('is-open');
+   loginInput.style.borderColor = '';
+}
+
+function authorized() {
+
+   function logOut() {
+      login = null;
+      localStorage.removeItem('myUsers')
+      buttonAuth.style.display = ''
+      userName.style.display = '';
+      buttonOut.style.display = '';
+
+      buttonOut.removeEventListener('click', logOut);
+      checkAuth();
+   }
+
+   console.log('Авторизован')
+
+   userName.textContent = login;
+
+   buttonAuth.style.display = 'none'
+   userName.style.display = 'inline';
+   buttonOut.style.display = 'block';
+
+   buttonOut.addEventListener('click', logOut);
+}
+
+function notAuthorized() {
+   console.log('Не авторизован');
+
+   function logIn(event) {
+      event.preventDefault();
+
+      if(loginInput.value.trim()){
+         login = loginInput.value;
+         localStorage.setItem('myUsers', login);
+         toggleModalAuth();
+         buttonAuth.removeEventListener('click', toggleModalAuth);
+         closeAuth.removeEventListener('click', toggleModalAuth);
+         loginForm.removeEventListener('submit', logIn);
+         loginForm.reset();
+         checkAuth();
+      } else {
+         loginInput.style.borderColor = 'red';
+         loginInput.placeholder = 'Введен неверный логин';
+      }
+
+   }
+
+   buttonAuth.addEventListener('click', toggleModalAuth);
+   closeAuth.addEventListener('click', toggleModalAuth);
+   loginForm.addEventListener('submit', logIn);
+}
+
+function checkAuth() {
+   if(login) {
+      authorized();
+   } else {
+      notAuthorized();
+   }
+}
+
+checkAuth();
